4efbc3a6189c7f799eabbe3e0018a9154b8c5fb8
David Mosen-Ansorena <dmosen.gn@cicbiogune.es>
2015-01-09 14:01:41 +0000

    Improved chromosome name detection in BAM files.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/seqCNA@98197 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/seqCNA.cpp
+++ src/seqCNA.cpp
@@ -26,6 +26,10 @@ int seqsumm(int* winv, std::string cmd, std::string outf) {
 								"chr7","chr8","chr9","chr10","chr11","chr12",
 								"chr13","chr14","chr15","chr16","chr17","chr18",
 								"chr19","chr20","chr21","chr22","chrX","chrY"};
+	std::string chrnum_str[chr_num] = {"1","2","3","4","5","6",
+								"7","8","9","10","11","12",
+								"13","14","15","16","17","18",
+								"19","20","21","22","X","Y"};
 	double chr_max_len[chr_num] = {	275e+06,268e+06,218e+06,211e+06,200e+06,189e+06,176e+06,162e+06,156e+06,150e+06,
 								149e+06,148e+06,127e+06,119e+06,113e+06,100e+06,90e+06,86e+06,66e+06,70e+06,53e+06,57e+06,
 								171e+06,66e+06}; // reserve an upper threshold of the required space (10% greater than expected)
@@ -89,7 +93,7 @@ int seqsumm(int* winv, std::string cmd, std::string outf) {
 		if (elems.size() < 10) goto next; // if so, not a SAM read line
 		chr = -1;
 		for (int i=0;i<chr_num;i++)
-			if (elems[2].compare(chr_str[i])==0) {
+			if (elems[2].compare(chr_str[i])==0 || elems[2].compare(chrnum_str[i])==0) {
 				chr = i;
 				goto nochr;
 			}
